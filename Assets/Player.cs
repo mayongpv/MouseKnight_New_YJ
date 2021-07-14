682dee65014925696840499bee4ee9d09496e3aa
YouJungHa <mayongpv@icloud.com>
2021-07-14 18:54:41 +0900

    우클릭 Jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,19 +8,51 @@ public class Player : MonoBehaviour
     public float moveableDistance = 3;
     public Transform mousePointer;
 
+    Plane plane = new Plane(new Vector3(0, 1, 0), 0);
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
+        Move();
+        Jump();
+
+    }
+
+    public AnimationCurve jumpYac; //jumpYac 점프할 때 y 애니메이션
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            mousePointer.position = hit.point;
-            float distance = Vector3.Distance(hit.point, transform.position); //두개 사이의 거리를 측정하겠다. 
+            StartCoroutine(JumpCo());
+        }
+    }
+    private IEnumerator JumpCo()
+    {
+        float jumpeStartTime = Time.time;
+        float jumpDuration = jumpYac[jumpYac.length - 1].time; 
+        float jumpEndTime = jumpeStartTime + jumpDuration;
+        float sumEvaluateTime = 0;
+        while (Time.time < jumpEndTime)
+        {
+            float y = jumpYac.Evaluate(sumEvaluateTime);
+            transform.Translate(0, y, 0);
+            yield return null;
+            sumEvaluateTime += Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            mousePointer.position = hitPoint;
+            float distance = Vector3.Distance(hitPoint, transform.position); //두개 사이의 거리를 측정하겠다. 
             if (distance > moveableDistance)
             {
-                var dir = hit.point - transform.position;
+                var dir = hitPoint - transform.position;
                 dir.Normalize();
-                transform.Translate(dir *speed* Time.deltaTime);  ; // 움직이니까 이렇게 써주고  dir이 움직이는 방향, 
+                transform.Translate(dir * speed * Time.deltaTime); ; // 움직이니까 이렇게 써주고  dir이 움직이는 방향, 
 
             }
         }
