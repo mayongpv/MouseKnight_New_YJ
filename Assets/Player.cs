229a80e090a8a33a55c753703ea492b6d4bd9096
YouJungHa <mayongpv@icloud.com>
2021-07-18 17:20:41 +0900

    Player - c

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -14,13 +14,11 @@ public class Player : MonoBehaviour
         state = StateType.NotBegin;
     }
 
-
     [SerializeField] StateType state = StateType.Idle;
-
     public float speed = 5;
     float normalSpeed;
-    public float walkDiastance = 12;
-    public float stopdistance = 7;
+    public float walkDistance = 12;
+    public float stopDistance = 7;
     public Transform mousePointer;
     public Transform spriteTr;
     Plane plane = new Plane(new Vector3(0, 1, 0), 0);
@@ -34,75 +32,76 @@ public class Player : MonoBehaviour
                 return;
 
             if (EditorOption.Options[OptionType.Player상태변화로그])
-                Debug.Log($"state:{state}=>value:{value}");
+                Debug.Log($"state:{state} => value:{value}");
 
             state = value;
             animator.Play(state.ToString());
         }
     }
-
-
     NavMeshAgent agent;
-
     private void Start()
     {
         normalSpeed = speed;
         animator = GetComponentInChildren<Animator>();
         spriteTr = GetComponentInChildren<SpriteRenderer>().transform;
         agent = GetComponent<NavMeshAgent>();
-        spriteTrailRenderer = GetComponentInChildren<SpritTrailRenderer>();
+        spriteTrailRenderer = GetComponentInChildren<SpriteTrailRenderer.SpriteTrailRenderer>();
         spriteTrailRenderer.enabled = false;
-
-
     }
 
     void Update()
     {
-        if (CanMoceState())
+        if (CanMoveState())
         {
             Move();
             Jump();
         }
 
         bool isSucceedDash = Dash();
+
         Attack(isSucceedDash);
     }
 
-    private bool CanMoceState()
+    private bool CanMoveState()
     {
         if (State == StateType.Attack)
             return false;
+
         if (State == StateType.TakeHit)
             return false;
+
         if (State == StateType.Death)
             return false;
 
         return true;
     }
+
     private void Attack(bool isSucceedDash)
     {
         if (isSucceedDash)
             return;
 
+        // 마우스 왼쪽 버튼 땠을때 공격 시키자.
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             StartCoroutine(AttackCo());
         }
-
     }
+
     public float attackTime = 1;
     public float attackApplyTime = 0.2f;
-    public LayerMask enermyLayer;
+    public LayerMask enemyLayer;
     public SphereCollider attackCollider;
     public float power = 10;
-
     private IEnumerator AttackCo()
     {
         State = StateType.Attack;
         yield return new WaitForSeconds(attackApplyTime);
+        //실제 어택하는 부분.
 
         Collider[] enemyColliders = Physics.OverlapSphere(
-            attackCollider.transform.position, attackCollider.radius.enemyLayer);
+            attackCollider.transform.position
+            , attackCollider.radius, enemyLayer);
         foreach (var item in enemyColliders)
         {
             item.GetComponent<Goblin>().TakeHit(power);
@@ -111,18 +110,20 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(attackTime);
         State = StateType.Idle;
     }
+
     [Foldout("대시")] public float dashableDistance = 10;
     [Foldout("대시")] public float dashableTime = 0.4f;
-
     float mouseDownTime;
     Vector3 mouseDownPosition;
     private bool Dash()
     {
+        // 마우스 드래그를 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             mouseDownTime = Time.time;
-            mouseDownPosition = Input.mousePosition;
+            mouseDownPosition = Input.mousePosition; // 
         }
+
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
@@ -137,53 +138,49 @@ public class Player : MonoBehaviour
         return false;
     }
 
-    [Foldout("대사")] public float dashTime = 0.3f;
+    [Foldout("대시")] public float dashTime = 0.3f;
 
     public float hp = 100;
-    internal void TakeHit(int damage)
+    internal void TakeHit(int damge)
     {
         if (State == StateType.Death)
             return;
 
-        hp -= damage;
+        hp -= damge;
         StartCoroutine(TakeHitCo());
-
     }
 
     public float takeHitTime = 0.3f;
     private IEnumerator TakeHitCo()
     {
-        State = StateType.TakeHit;
+        State = StateType.TakeHit; //피격 모션하자.
         yield return new WaitForSeconds(takeHitTime);
 
         if (hp > 0)
             State = StateType.Idle;
         else
-            StartCoroutine(DeathCo());
+            StartCoroutine(DeathCo());//hp < 0 으면 죽자.
     }
 
     public float deathTime = 0.5f;
-
     private IEnumerator DeathCo()
     {
-        State = StateType.Death;
+        State = StateType.Death; //피격 모션하자.
         yield return new WaitForSeconds(deathTime);
-        Debug.LogWarning("게임종료");
+        Debug.LogWarning("게임 종료");
     }
-
     SpriteTrailRenderer.SpriteTrailRenderer spriteTrailRenderer;
     [Foldout("대시")] public float dashSpeedMultiplySpeed = 4f;
     Vector3 dashDirection;
     private IEnumerator DashCo()
     {
-        /* 방향을 바꿀 수 없게 -> 진행방향으로 이동 -> 대각선으로 이동-> 드래그 방향 이동할건지
-         * 플레이어 이동방향 X 이동할건지
-         * DashDirection x 방향만 사용
-         */
+        //방향을 바꿀 수 없게끔, -> 진행방향으로 이동 -> 대각선 이동 대각선이동 -> 드래그방향으로 이동할건지
+        //    플레이이어이동방향 x이동할 껀지
+        //// dashDirection x방향만 사용.
         spriteTrailRenderer.enabled = true;
         dashDirection = Input.mousePosition - mouseDownPosition;
         dashDirection.y = 0;
-        dashDirection.x = 0;
+        dashDirection.z = 0;
         dashDirection.Normalize();
         speed = normalSpeed * dashSpeedMultiplySpeed;
         State = StateType.Dash;
@@ -193,13 +190,14 @@ public class Player : MonoBehaviour
         spriteTrailRenderer.enabled = false;
     }
 
-    private bool isSucceesDashDrag()
+    private bool IsSucceesDashDrag()
     {
-        //시간 체크
+        // 시간 체크.
         float dragTime = Time.time - mouseDownTime;
         if (dragTime > dashableTime)
             return false;
 
+        // 거리체크.
         float dragDistance = Vector3.Distance(mouseDownPosition, Input.mousePosition);
         if (dragDistance < dashableDistance)
             return false;
@@ -207,18 +205,17 @@ public class Player : MonoBehaviour
         return true;
     }
 
-
-    [BoxGroup("Jump")] public AnimationCurve jumpYac; //jumpYac 점프할 때 y 애니메이션
+    [BoxGroup("점프")] public AnimationCurve jumpYac;
     private void Jump()
     {
-        if (jumpState == JumpStateType.Jump) // 상태가 점프면 점프 못하도록 - > 나간다
+        if (jumpState == JumpStateType.Jump)
             return;
-        if (Input.GetKeyDown(KeyCode.Mouse1))// 즉 점프중에는 이 코드가 실행이 안됨
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             StartCoroutine(JumpCo());
         }
     }
-    public enum JumpStateType // 점프중이라면 점프 못하게 하려고 추가함. 
+    public enum JumpStateType
     {
         Ground,
         Jump,
@@ -236,23 +233,21 @@ public class Player : MonoBehaviour
         Death
     }
 
-    Animator animatior;
-    JumpStateType jumpStateType;
+    Animator animator;
+    JumpStateType jumpState;
     [BoxGroup("점프")] public float jumpYMultiply = 1;
     [BoxGroup("점프")] public float jumpTimeMultiply = 1;
-
     private IEnumerator JumpCo()
     {
-        jumpStateType = JumpStateType.Jump;
+        jumpState = JumpStateType.Jump;
         State = StateType.JumpUp;
-        float jumpStateTime = Time.time;
+        float jumpStartTime = Time.time;
         float jumpDuration = jumpYac[jumpYac.length - 1].time;
         jumpDuration *= jumpTimeMultiply;
         float jumpEndTime = jumpStartTime + jumpDuration;
         float sumEvaluateTime = 0;
         float previousY = float.MinValue;
         agent.enabled = false;
-
         while (Time.time < jumpEndTime)
         {
             float y = jumpYac.Evaluate(sumEvaluateTime / jumpTimeMultiply);
@@ -262,8 +257,10 @@ public class Player : MonoBehaviour
 
             if (previousY > transform.position.y)
             {
+                //떨어지는 모션으로 바꾸자.
                 State = StateType.JumpDown;
             }
+
             if (transform.position.y < 0)
             {
                 break;
@@ -272,13 +269,12 @@ public class Player : MonoBehaviour
             previousY = transform.position.y;
             sumEvaluateTime += Time.deltaTime;
         }
-        agent.enabld = true;
+        agent.enabled = true;
         jumpState = JumpStateType.Ground;
         State = StateType.Idle;
-
     }
 
-    private void Move() // 마우스 클릭으로 움직이는 클래스
+    private void Move()
     {
         if (Time.timeScale == 0)
             return;
@@ -289,22 +285,25 @@ public class Player : MonoBehaviour
         {
             Vector3 hitPoint = ray.GetPoint(enter);
             mousePointer.position = hitPoint;
-            float distance = Vector3.Distance(hitPoint, transform.position); //두개 사이의 거리를 측정하겠다. 
+            float distance = Vector3.Distance(hitPoint, transform.position);
 
-            float movableDistance = stopDistance;
+            float movealbeDistance = stopDistance;
             // State가 Walk 일땐 7(stopDistance)사용.
             // Idle에서 Walk로 갈땐 12(WalkDistance)사용
             if (State == StateType.Idle)
-                movableDistance = walkDistance;
+                movealbeDistance = walkDistance;
 
-            var dir = hitPoint - transform.position;
+            Vector3 dir = hitPoint - transform.position;
 
             if (State == StateType.Dash)
-                dir.Normalize();
+                dir = dashDirection;
 
-            if (distance > movableDistance || State == StateType.Dash)
+            dir.Normalize();
+
+            if (distance > movealbeDistance || State == StateType.Dash)
             {
-                transform.Translate(dir * speed * Time.deltaTime, Space.World); ; // 움직이니까 이렇게 써주고  dir이 움직이는 방향, 
+                transform.Translate(dir * speed * Time.deltaTime, Space.World);
+
                 if (ChangeableState())
                     State = StateType.Walk;
             }
@@ -317,7 +316,6 @@ public class Player : MonoBehaviour
             //방향(dir)에 따라서
             //오른쪽이라면 Y : 0
             //왼쪽이라면 Y : 180
-
             bool isRightSide = dir.x > 0;
             if (isRightSide)
             {
@@ -341,5 +339,3 @@ public class Player : MonoBehaviour
         }
     }
 }
-
-
